b763adc77a162c3659c50e701449c3e1d730bbe2
Erik Bryant <erikbryantology@gmail.com>
2017-12-27 22:39:15 -0800

    Move data cleanup out of plotting routines.

--- plot.py
+++ plot.py
@@ -2,15 +2,30 @@ import csv
 
 import matplotlib.pyplot as plt
 
+y = []
+c = []
+i = 0
+
 # "InvoiceNo","StockCode","Description","Quantity","InvoiceDate","UnitPrice","CustomerID","Country"
-with open('online_retail.csv', newline='') as csvfile:
+with open('clean_online_retail.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        row['InvoiceNo'] = int(float(row['InvoiceNo']))
+        i += 1
+        if i > 100:
+            break
+        quantity = float(row['Quantity'])
+        unitprice = float(row['UnitPrice'])
+        if row['InvoiceNo'][0] == 'C':
+            continue
+        if quantity < 0:
+            continue
+        if quantity > 10000:
+            continue
+        y.append(quantity)
+        c.append(quantity * unitprice)
 
-for r in range(10):
-    print(row[r]['InvoiceNo'])
-
+x = range(len(y))
+print("len = %s" % len(y))
 plt.plot(x, y)
-
+plt.plot(x, c)
 plt.show()
--- xlstocsv.py
+++ xlstocsv.py
@@ -1,11 +1,18 @@
 import xlrd
 import csv
 
-def csv_from_excel():
+def unfloat(cell):
+    try:
+        cell = int(float(cell))
+    except:
+        pass
+    return cell
+
+def csv_from_excel(csv_file):
     wb = xlrd.open_workbook('Online Retail.xlsx')
     print("Sheet names: %s" % wb.sheet_names())
     sh = wb.sheet_by_name('Online Retail')
-    your_csv_file = open('online_retail.csv', 'w')
+    your_csv_file = open(csv_file, 'w')
     wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
 
     for rownum in range(sh.nrows):
@@ -13,5 +20,33 @@ def csv_from_excel():
 
     your_csv_file.close()
 
-# runs the csv_from_excel function:
-csv_from_excel()
+def clean_csv(csv_file):
+    out_csv_file = open('clean_' + csv_file, 'w')
+    fieldnames = [
+            "InvoiceNo",
+            "StockCode",
+            "Description",
+            "Quantity",
+            "InvoiceDate",
+            "UnitPrice",
+            "CustomerID",
+            "Country"
+            ]
+    wr = csv.DictWriter(f=out_csv_file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+    wr.writeheader()
+
+    # "InvoiceNo","StockCode","Description","Quantity","InvoiceDate","UnitPrice","CustomerID","Country"
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            row['InvoiceNo'] = unfloat(row['InvoiceNo'])
+            row['StockCode'] = unfloat(row['StockCode'])
+            row['CustomerID'] = unfloat(row['CustomerID'])
+            row['InvoiceDate'] = int((float(row['InvoiceDate']) - 25569) * 86400)
+            wr.writerow(row)
+
+    out_csv_file.close()
+
+csv_file = 'online_retail.csv'
+csv_from_excel(csv_file)
+clean_csv(csv_file)
